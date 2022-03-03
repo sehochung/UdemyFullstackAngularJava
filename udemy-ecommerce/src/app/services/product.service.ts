44109557238694ebedbf6f21591213573e8c8e1c
seho <schung68@wisc.edu>
2022-03-03 12:02:47 -0500

    Finished section 15

--- udemy-ecommerce/src/app/services/product.service.ts
+++ udemy-ecommerce/src/app/services/product.service.ts
@@ -11,6 +11,13 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
+  getProduct(theProductId: number): Observable<Product> {
+
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    return this.httpClient.get<Product>(productUrl);
+    
+
+  }
 
 
   private baseUrl = 'http://localhost:8080/api/products';
